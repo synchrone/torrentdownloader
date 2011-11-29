ba5bd2e9b973c96cfe8fecf8ab96282656b256e1
Alexander Bogdanov <syn@li.ru>
2011-11-29 18:52:51 +0400

    Initial commit

---
+++ frmUI.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TorrentDownloader
+{
+    partial class frmUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barProgress = new System.Windows.Forms.ProgressBar();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // barProgress
+            // 
+            this.barProgress.Location = new System.Drawing.Point(12, 12);
+            this.barProgress.Name = "barProgress";
+            this.barProgress.Size = new System.Drawing.Size(260, 23);
+            this.barProgress.TabIndex = 0;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInfo.Location = new System.Drawing.Point(281, 12);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(41, 23);
+            this.btnInfo.TabIndex = 1;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // lstLog
+            // 
+            this.lstLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 41);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(310, 95);
+            this.lstLog.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(121, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // frmUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 148);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.barProgress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmUI";
+            this.Text = "Torrent Downloader for ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barProgress;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmUI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using MonoTorrent.Common;
+using MonoTorrent.Client;
+
+namespace TorrentDownloader
+{
+    public partial class frmUI : Form
+    {
+        protected Timer timer = new Timer();
+
+        public frmUI() {
+            InitializeComponent();
+
+            if (Program.tm == null) { throw new Exception("TorrentManager is not instantiated"); }
+            this.Text += Program.tm.Torrent.Name;
+
+            Program.tm.TorrentStateChanged += new EventHandler<TorrentStateChangedEventArgs>(tm_TorrentStateChanged);
+            Program.tm.PieceHashed += new EventHandler<PieceHashedEventArgs>(tm_PieceHashed);
+            Program.tm.PeersFound += new EventHandler<PeersAddedEventArgs>(tm_PeersFound);
+            Program.tm.PeerDisconnected += new EventHandler<PeerConnectionEventArgs>(tm_PeerDisconnected);
+            Program.tm.PeerConnected += new EventHandler<PeerConnectionEventArgs>(tm_PeerConnected);
+
+            timer.Interval = 500;
+            timer.Tick+=new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            this.barProgress.Value = (int)Program.tm.Progress;
+        } 
+
+        void tm_PeerConnected(object sender, PeerConnectionEventArgs e)
+        {
+            
+        }
+
+        void tm_PeerDisconnected(object sender, PeerConnectionEventArgs e)
+        {
+            
+        }
+     
+        void tm_PeersFound(object sender, PeersAddedEventArgs e)
+        {
+            
+        }
+
+        void tm_PieceHashed(object sender, PieceHashedEventArgs e)
+        {
+            
+        }
+
+        void tm_TorrentStateChanged(object sender, TorrentStateChangedEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
