55bc1af3ac1d636dbf71af6190165d3f517dbe09
Alexander Bogdanov <syn@li.ru>
2011-11-30 00:24:26 +0400

    MonoTorrent referenced as embedded assembly

--- Program.cs
+++ Program.cs
@@ -4,95 +4,30 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Text;
 using System.IO;
-using MonoTorrent.Client;
-using MonoTorrent.Common;
+using System.Reflection;
 
 namespace TorrentDownloader
 {
     static class Program
     {
-        public static string boundary = "=TD"+"=B";
-        public static TorrentManager tm;
-        public static ClientEngine te;
         [STAThread]
         static void Main()
         {
-            byte[] exeBytes = File.ReadAllBytes(Application.ExecutablePath);
-            byte[] boundaryBytes = Encoding.ASCII.GetBytes(Program.boundary);
-            int torrentBeginsAt = FindSubArray(exeBytes, boundaryBytes, 0, exeBytes.Length);
-
-            if (torrentBeginsAt == -1) {
-                MessageBox.Show("Couldn't find Boundary: "+Program.boundary);
-                return;
-            }
-            torrentBeginsAt += boundaryBytes.Length;
-            byte[] torrentBytes = new byte[exeBytes.Length - torrentBeginsAt] ;
-            Array.Copy(exeBytes, torrentBeginsAt, torrentBytes, 0,torrentBytes.Length);
-
-            Torrent t = Torrent.Load(torrentBytes);    
-     
-            string dirName = t.Name;
-            foreach (char wrongChar in Path.GetInvalidFileNameChars())
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
             {
-                dirName.Replace(wrongChar, '_');
-            }
-            Program.te = new ClientEngine(new EngineSettings());
-            Program.tm = new TorrentManager(t, Path.Combine(Application.ExecutablePath, dirName), new TorrentSettings());
-            Program.te.Register(Program.tm);
 
-            Program.tm.Start();
+                String resourceName = String.Format("{0}.{1}.dll", typeof(Program).Namespace, new AssemblyName(args.Name).Name);
 
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmUI());
-        }
-
-        /// <summary>
-        /// Find subarray in the source array.
-        /// </summary>
-        /// 
-        /// <param name="array">Source array to search for needle.</param>
-        /// <param name="needle">Needle we are searching for.</param>
-        /// <param name="startIndex">Start index in source array.</param>
-        /// <param name="sourceLength">Number of bytes in source array, where the needle is searched for.</param>
-        /// 
-        /// <returns>Returns starting position of the needle if it was found or <b>-1</b> otherwise.</returns>
-        /// 
-        public static int FindSubArray(byte[] array, byte[] needle, int startIndex, int sourceLength)
-        {
-            int needleLen = needle.Length;
-            int index;
-
-            while (sourceLength >= needleLen)
-            {
-                // find needle's starting element
-                index = Array.IndexOf(array, needle[0], startIndex, sourceLength - needleLen + 1);
-
-                // if we did not find even the first element of the needls, then the search is failed
-                if (index == -1)
-                    return -1;
-
-                int i, p;
-                // check for needle
-                for (i = 0, p = index; i < needleLen; i++, p++)
-                {
-                    if (array[p] != needle[i])
-                    {
-                        break;
-                    }
-                }
-
-                if (i == needleLen)
-                {
-                    // needle was found
-                    return index;
-                }
-
-                // continue to search for needle
-                sourceLength -= (index - startIndex + 1);
-                startIndex = index + 1;
-            }
-            return -1;
         }
     }
 }
--- frmUI.cs
+++ frmUI.cs
@@ -9,63 +9,220 @@ using System.Windows.Forms;
 using System.IO;
 using MonoTorrent.Common;
 using MonoTorrent.Client;
+using System.Resources;
+using MonoTorrent.BEncoding;
 
 namespace TorrentDownloader
 {
     public partial class frmUI : Form
     {
-        protected Timer timer = new Timer();
+        public string boundary = "=TD" + "=B";
+        public string fastResumeFileName = "fastresume.dat";
+        
+        public TorrentManager tm;
+        public ClientEngine te;
+        public ComponentResourceManager Resources = new ComponentResourceManager(typeof(frmUI));
 
+        /// <summary>
+        /// Find subarray in the source array.
+        /// </summary>
+        /// 
+        /// <param name="array">Source array to search for needle.</param>
+        /// <param name="needle">Needle we are searching for.</param>
+        /// <param name="startIndex">Start index in source array.</param>
+        /// <param name="sourceLength">Number of bytes in source array, where the needle is searched for.</param>
+        /// 
+        /// <returns>Returns starting position of the needle if it was found or <b>-1</b> otherwise.</returns>
+        /// 
+        private static int FindSubArray(byte[] array, byte[] needle, int startIndex, int sourceLength)
+        {
+            int needleLen = needle.Length;
+            int index;
+
+            while (sourceLength >= needleLen)
+            {
+                // find needle's starting element
+                index = Array.IndexOf(array, needle[0], startIndex, sourceLength - needleLen + 1);
+
+                // if we did not find even the first element of the needls, then the search is failed
+                if (index == -1)
+                    return -1;
+
+                int i, p;
+                // check for needle
+                for (i = 0, p = index; i < needleLen; i++, p++)
+                {
+                    if (array[p] != needle[i])
+                    {
+                        break;
+                    }
+                }
+
+                if (i == needleLen)
+                {
+                    // needle was found
+                    return index;
+                }
+
+                // continue to search for needle
+                sourceLength -= (index - startIndex + 1);
+                startIndex = index + 1;
+            }
+            return -1;
+        }
+    
         public frmUI() {
             InitializeComponent();
-
-            if (Program.tm == null) { throw new Exception("TorrentManager is not instantiated"); }
-            this.Text += Program.tm.Torrent.Name;
-
-            Program.tm.TorrentStateChanged += new EventHandler<TorrentStateChangedEventArgs>(tm_TorrentStateChanged);
-            Program.tm.PieceHashed += new EventHandler<PieceHashedEventArgs>(tm_PieceHashed);
-            Program.tm.PeersFound += new EventHandler<PeersAddedEventArgs>(tm_PeersFound);
-            Program.tm.PeerDisconnected += new EventHandler<PeerConnectionEventArgs>(tm_PeerDisconnected);
-            Program.tm.PeerConnected += new EventHandler<PeerConnectionEventArgs>(tm_PeerConnected);
-
-            timer.Interval = 500;
-            timer.Tick+=new EventHandler(timer_Tick);
-            timer.Start();
+        }
+        protected string FormatSize(long bytes) { 
+            
+            if( bytes > 1024 && bytes < (1048576) ){ //1024^2
+                return Math.Round(((decimal)bytes / 1024), 1) + " "+Resources.GetString("KB/s");
+            }
+            else if (bytes > (1048576) && bytes < (1073741824)){ //1024^3
+                return Math.Round(((decimal)bytes / (1048576)), 1) + " " + Resources.GetString("MB/s");
+            }
+            else if (bytes > (1073741824) && bytes < (1099511627776)) {//1024^4
+                return Math.Round(((decimal)bytes / (1073741824)), 1) + " " + Resources.GetString("GB/s");
+            }
+            else if( bytes > (1099511627776 ) ){
+                return Math.Round(((decimal)bytes / (1099511627776)), 1) + " " + Resources.GetString("TB/s");
+            }
+            else{
+                return bytes + " " + Resources.GetString("bytes/s"); ;
+            }
+        }
+        void LoggerHandler(object sender, EventArgs e) {
+            if (lstLog.InvokeRequired) {
+                Action wrapper = delegate(){LoggerHandler(sender,e);};
+                lstLog.Invoke(wrapper);
+                return;
+            }
+            if (e is StatsUpdateEventArgs) {
+                this.barProgress.Value = (int)this.tm.Progress;
+                this.lblProgress.Text = String.Format("{0}%", this.tm.Progress);
+                this.lblSpeed.Text = String.Format("{0}", FormatSize(this.tm.Monitor.DownloadSpeed));
+                this.lblPeersCount.Text = String.Format("{0}/{1}", this.tm.Peers.Seeds, this.tm.Peers.Leechs);
+            }
+            else if (e is PeerConnectionEventArgs)
+            {
+                lstLog.Items.Add(String.Format("Peer {0} connected", ((PeerConnectionEventArgs)e).PeerID));
+            }else if (e is PeerConnectionEventArgs)
+            {
+                lstLog.Items.Add(String.Format("Peer {0} disconnected", ((PeerConnectionEventArgs)e).PeerID));
+            }else if (e is PeersAddedEventArgs)
+            {
+                lstLog.Items.Add(String.Format("{0} new peers found", ((PeersAddedEventArgs)e).NewPeers));
+            }else if (e is PieceHashedEventArgs)
+            {
+                lstLog.Items.Add(String.Format("Piece #{0} hashed", ((PieceHashedEventArgs)e).PieceIndex));
+            }else if (e is TorrentStateChangedEventArgs)
+            {
+                lstLog.Items.Add(String.Format("State changed to {0}", 
+                    Enum.GetName(typeof(TorrentState), 
+                        ((TorrentStateChangedEventArgs)e).NewState)
+                    )
+                );
+            }
         }
 
-        void timer_Tick(object sender, EventArgs e)
+        private void frmUI_Load(object sender, EventArgs e)
         {
-            this.barProgress.Value = (int)Program.tm.Progress;
-        } 
+            byte[] exeBytes = File.ReadAllBytes(Application.ExecutablePath);
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes(this.boundary);
+            int torrentBeginsAt = FindSubArray(exeBytes, boundaryBytes, 0, exeBytes.Length);
 
-        void tm_PeerConnected(object sender, PeerConnectionEventArgs e)
-        {
-            
+            if (torrentBeginsAt == -1)
+            {
+                MessageBox.Show(Resources.GetString("cannot_find_boundary"));
+                this.Close();
+            }
+            torrentBeginsAt += boundaryBytes.Length;
+            byte[] torrentBytes = new byte[exeBytes.Length - torrentBeginsAt];
+            Array.Copy(exeBytes, torrentBeginsAt, torrentBytes, 0, torrentBytes.Length);
+
+            try
+            {
+                Torrent t = Torrent.Load(torrentBytes);
+                string dirName = t.Name;
+                foreach (char wrongChar in Path.GetInvalidFileNameChars())
+                {
+                    dirName.Replace(wrongChar, '_');
+                }
+                dirName = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), dirName);
+
+                this.te = new ClientEngine(new EngineSettings());
+                this.tm = new TorrentManager(t, dirName, new TorrentSettings());
+
+                string newDirName = dirName;
+                int i = 0;
+                while (File.Exists(newDirName))
+                {
+                    newDirName = dirName + "_" + i++;
+                }
+                dirName = newDirName;
+
+                if (!Directory.Exists(dirName))
+                {
+                    Directory.CreateDirectory(dirName);
+                }
+                else if (File.Exists(Path.Combine(dirName, fastResumeFileName)))
+                {
+                    tm.LoadFastResume(new FastResume(
+                        BEncodedDictionary.Decode<BEncodedDictionary>(
+                            File.ReadAllBytes(
+                                Path.Combine(dirName, fastResumeFileName)
+                            )
+                        )
+                    ));
+                }
+
+                this.te.Register(this.tm);
+            }
+            catch (BEncodingException)
+            {
+                MessageBox.Show(Resources.GetString("torrent_couldnt_be_read"));
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Resources.GetString("unknown_error") + Environment.NewLine + ex.Message);
+                this.Close();
+            }
+
+            if (this.tm == null) { throw new Exception("TorrentManager is not instantiated"); }
+            this.Text += this.tm.Torrent.Name;
+
+            this.tm.TorrentStateChanged += new EventHandler<TorrentStateChangedEventArgs>(LoggerHandler);
+            this.tm.PieceHashed += new EventHandler<PieceHashedEventArgs>(LoggerHandler);
+            this.tm.PeersFound += new EventHandler<PeersAddedEventArgs>(LoggerHandler);
+            this.tm.PeerDisconnected += new EventHandler<PeerConnectionEventArgs>(LoggerHandler);
+            this.tm.PeerConnected += new EventHandler<PeerConnectionEventArgs>(LoggerHandler);
+
+            this.te.StatsUpdate += new EventHandler<StatsUpdateEventArgs>(LoggerHandler);
+            this.tm.Start();
         }
 
-        void tm_PeerDisconnected(object sender, PeerConnectionEventArgs e)
+        private void frmUI_FormClosing(object sender, FormClosingEventArgs e)
         {
-            
-        }
-     
-        void tm_PeersFound(object sender, PeersAddedEventArgs e)
-        {
-            
-        }
-
-        void tm_PieceHashed(object sender, PieceHashedEventArgs e)
-        {
-            
+            Console.Write(true);
+            try
+            {
+                FastResume fr = this.tm.SaveFastResume();
+                File.WriteAllBytes(Path.Combine(this.tm.SavePath, fastResumeFileName), fr.Encode().Encode());
+            }
+            catch { }
         }
 
-        void tm_TorrentStateChanged(object sender, TorrentStateChangedEventArgs e)
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
         {
-            
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
+            if (this.Height == 350)
+            {
+                this.Height = 95;
+            }
+            else {
+                this.Height = 350;
+            }
         }
     }
 }
